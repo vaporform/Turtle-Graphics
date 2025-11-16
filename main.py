0b6bb46035bf4ee33237c8369cada603b361ac14
vaporform <ysktyrch@gmail.com>
2025-11-16 16:11:33 +0700

    Added methods to the main app.

--- main.py
+++ main.py
@@ -1,7 +1,92 @@
+from oop_helper import *
 
+class App(Drawer):
+    def __init__(self, ):
+        # Initialize the app, canvas, and turtle.
+        super(App,self).__init__()
+        # Create some stuff...
+        self.option = None
+        self.amount = None
+        self.reduction_ratio = None
 
+    def ask(self):
+        # Ask the user.
+        while True:
+            op = input("Which art do you want to generate? Enter a number between 1 to 9 inclusive: ")
+            try:
+                op = int(op)
+                if 1 <= op <= 9:
+                    self.option = op
+                    break
+                raise 
+            except:
+                print("Invalid choice! Try again.")
+        
+    def show_canvas(self):
+        # Finish and show the results.
+        turtle.done()
 
+    def rand(self,a,b):
+        # Return some random variable from a to b.
+        return random.randint(a,b)
+    
+    def create_polygons(self,amount,sides):
+        polygons = []
+        for _ in range(amount):
+            size = random.randint(50, 150)
+            orientation = random.randint(0, 90)
+            location = [random.randint(-300, 300), random.randint(-200, 200)]
+            polygons.append(Polygon(sides,size,orientation,location))  
+        return polygons
+    
+    def generate(self):
+        option = self.option
+        # Configs!
+        # NORMALS: 1 2 3 [4]
+        # DOUBLES: 5 6 7 [8]
+        # MIX: 9
+        # Triangle:1,5, Rectangle:2,6, Hexagon:3,7
+        # 4,8,9 are all of shapes.
 
-option = input("Which art do you want to generate? Enter a number between 1 to 9 inclusive: ")
-if option == '1':
-    pass
+        turtle.speed(0)
+        turtle.bgcolor('black')
+        turtle.tracer(0)
+        turtle.colormode(255)
+
+        self.amount = self.rand(10,20)
+        # Grab some polys.
+
+        if option in (1,4,9):
+            for i in self.create_polygons(self.amount,3):
+                self.random_brush()
+                self.draw_polygon(i)
+        if option in (2,4,9):
+            for i in self.create_polygons(self.amount,4):
+                self.random_brush()
+                self.draw_polygon(i)
+        if option in (3,4,9):
+            for i in self.create_polygons(self.amount,5):
+                self.random_brush()
+                self.draw_polygon(i)
+        
+        if option in (5,8,9):
+            for i in self.create_polygons(self.amount,3):
+                self.random_brush()
+                self.draw_n(3,i,0.618)
+        if option in (6,8,9):
+            for i in self.create_polygons(self.amount,4):
+                self.random_brush()
+                self.draw_n(3,i,0.618)
+        if option in (7,8,9):
+            for i in self.create_polygons(self.amount,5):
+                self.random_brush()
+                self.draw_n(3,i,0.618)
+        
+        
+        # hold the window; close it by clicking the window close 'x' mark
+        turtle.done()
+
+# RUN THE PROGRAM!
+Win = App()
+Win.ask()
+Win.generate()
--- oop_helper.py
+++ oop_helper.py
@@ -2,39 +2,51 @@ import turtle
 import random
 
 class Polygon:
-    def __init__(self,sides,size,orientation,location,color,border):
+    def __init__(self,sides,size,orientation,location):
         # Initializes the polygon
         self.num_sides = sides
         self.size = size
         self.orientation = orientation
         self.location = location
-        self.color = color
-        self.border_size = border
 
 class Drawer:
     def __init__(self):
         self.location = [0.0,0.0]
+        self.color = None
+        self.border_size = None
 
-    def draw_polygon(self,polygon: Polygon, reduction_ratio=1):
+    def draw_polygon(self,polygon: Polygon):
         turtle.penup()
         turtle.goto(polygon.location[0], polygon.location[1])
         turtle.setheading(polygon.orientation)
-        turtle.color(polygon.color)
-        turtle.pensize(polygon.border_size)
+        turtle.color(self.color)
+        turtle.pensize(self.border_size)
         turtle.pendown()
         for _ in range(polygon.num_sides):
-            turtle.forward(polygon.size * reduction_ratio)
+            turtle.forward(polygon.size)
             turtle.left(360/polygon.num_sides)
         turtle.penup()
+        self.location = self.location = [turtle.pos()[0],turtle.pos()[1]]
 
     def get_new_color(self):
         return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     
-    def reposition(self,size,reduction_ratio):
-        # reposition the turtle and get a new location
+    def reposition_inside(self,size,reduction_ratio):
+        # reposition the turtle into a polygon and get a new location
         turtle.penup()
         turtle.forward(size*(1-reduction_ratio)/2)
         turtle.left(90)
         turtle.forward(size*(1-reduction_ratio)/2)
         turtle.right(90)
         self.location = [turtle.pos()[0],turtle.pos()[1]]
+
+    def random_brush(self):
+        self.color = self.get_new_color()
+        self.border_size = random.randint(1, 10)
+    
+    def draw_n(self,n, polygon: Polygon, reduction_ratio):
+        for i in range(n):
+            polygon.location = self.location
+            polygon.size = polygon.size * (reduction_ratio**i)
+            self.draw_polygon(polygon)
+            self.reposition_inside(polygon.size,reduction_ratio)
